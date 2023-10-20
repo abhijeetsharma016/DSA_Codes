83da72b1682660c92934f965a714d150835b32b4
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2023-10-20 17:11:59 +0530

    Q8

---
+++ src/com/GFG/BitMagic/Q8_OccuringOfOddNumberOfTime.java
@@ -0,0 +1,15 @@
+package com.GFG.BitMagic;
+
+public class Q8_OccuringOfOddNumberOfTime {
+    public static void main(String[] args) {
+        int arr [] = {2,2,5,5,5,5,10,4,4,3,3};
+        System.out.println(findOddOccurance(arr));
+    }
+    public static int findOddOccurance(int [] n){
+        int result =0;
+        for(int i=0; i<n.length;i++){
+            result = result^n[i];
+        }
+        return result;
+    }
+}
