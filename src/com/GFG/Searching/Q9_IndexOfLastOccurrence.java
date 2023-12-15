d9d852c1eebfc03b046c1906b31ffb5954c9d415
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2023-12-15 23:36:55 +0530

    Q9_IndexOfLastOccurrence

---
+++ src/com/GFG/Searching/Q9_IndexOfLastOccurrence.java
@@ -0,0 +1,39 @@
+package com.GFG.Searching;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q9_IndexOfLastOccurrence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements: ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+
+        for(int i = 0; i<size; i++){
+            System.out.print("Enter the " + (i+1) + " element: ");
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
+        System.out.print("Enter the element to be searched: ");
+        int element = sc.nextInt();
+        System.out.println("First one occur at: " + LastOccurrence(arr,size,element));
+    }
+    public static int LastOccurrence(int[] arr, int size, int element){
+        int l = 0;
+        int r = size-1;
+        while (l<=r){
+            int mid = (l+r)/2;
+            if(arr[mid]==element &&(mid==size-1 || arr[mid+1]!=arr[mid])){
+                return mid;
+            }
+            if(arr[mid]<element || (mid!=size-1 && arr[mid+1]==arr[mid])){
+                l = mid+1;
+            }
+            else{
+                r = mid-1;
+            }
+        }
+        return -1;
+    }
+}
