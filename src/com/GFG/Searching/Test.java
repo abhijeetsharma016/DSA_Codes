c8eadb7a9e8e00bcf2f1e5cf2675fb1ded8679f9
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2023-12-20 02:28:05 +0530

    just added a test class to check something

---
+++ src/com/GFG/Searching/Test.java
@@ -0,0 +1,13 @@
+package com.GFG.Searching;
+
+public class Test {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+        changeValue(a);
+        System.out.println(a);
+    }
+    public static void changeValue(int a){
+        a = 100;
+    }
+}
