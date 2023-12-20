a71cc3aa23f4a08d32a4c04c8a907785b1c44866
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2023-12-20 23:18:23 +0530

    Q13_SearchingInRotatedArray

--- src/com/GFG/Searching/Q13_SearchingInARotatedArray.java
+++ src/com/GFG/Searching/Q13_SearchingInARotatedArray.java
@@ -1,51 +1,57 @@
 package com.GFG.Searching;
 
 import java.lang.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 
+class Q13_SearchingInARotatedArray
+{ public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number of elements: ");
+    int size = sc.nextInt();
+    int[] arr = new int[size];
 
-class Q12_SearchinginARotatedArray
-{
+    for(int i = 0; i<size; i++){
+        System.out.print("Enter the " + (i+1) + " element: ");
+        arr[i] = sc.nextInt();
+    }
+    System.out.println(Arrays.toString(arr));
+    System.out.print("Enter the element to be searched: ");
+    int element = sc.nextInt();
+    System.out.println("element found at: " + searchInRotatedArr(arr,size ,element));
+    }
 
-    static int search(int arr[], int n, int x)
-    {
-        int low = 0, high = n - 1;
+    public static int searchInRotatedArr(int[] arr, int size, int element) {
+    int low = 0;
+    int high = size-1;
+        while (low < high) {
+            int mid = (low+high)/2;
 
-        while(low <= high)
-        {
-            int mid = (low + high) / 2;
-
-            if(arr[mid] == x)
+            if(arr[mid]== element){
                 return mid;
-            if(arr[low] <= arr[mid])
-            {
-                if(x >= arr[low] && x < arr[mid])
-                    high = mid - 1;
-                else
-                    low = mid + 1;
             }
-            else
-            {
-                if(x > arr[mid] && x <= arr[high])
-                    low = mid + 1;
-                else
-                    high = mid - 1;
+
+
+            if(arr[low]<arr[mid]){
+                //left half is sorted
+                if(arr[mid]>= element && arr[low]<= element){//element is present in left half
+                    high = mid-1;
+                }
+                else{
+                    low = mid+1;
+                }
+            }
+            else{//right half is sorted
+                if(arr[mid]<= element && arr[high]>= element){//arr lies in right half
+                    low = mid+1;
+                }
+                else{// arr lies in left half
+                    high = mid-1;
+                }
+
             }
         }
-
-
         return -1;
     }
-
-    public static void main(String args[])
-    {
-
-        int arr[] = {10, 20, 40, 60, 5, 8}, n = 6;
-
-        int x = 5;
-
-        System.out.println(search(arr, n, x));
-
-    }
-
 }
