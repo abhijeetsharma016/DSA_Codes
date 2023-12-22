a643e026d11f770df70cb65937856892de33c432
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2023-12-22 19:36:15 +0530

    error

---
+++ src/com/GFG/Searching/Q17_MedianOfTwoSortedArray.java
@@ -0,0 +1,26 @@
+/*
+package com.GFG.Searching;
+
+public class Q17_MedianOfTwoSortedArray {
+    public static void main(String[] args) {
+        int[] arr1 = {-5, 3, 6, 12, 15};
+        int[] arr2 = {-12, -10, -6, -3, 4, 10};
+        System.out.println("Median of two arrays are: ");
+        System.out.println(Median(arr1,arr2));
+    }
+    public static int Median(int[] arr1, int[] arr2){
+        int size1 = arr1.length;
+        int size2 = arr2.length;
+        if(size1>size2){
+            return Median(arr2,arr1);//Swapping
+        }//2nd array should be greater than first one
+
+        int start = 0;
+        int end = size1;
+        int realMidInMergedArray = (size1+size2+1)/2;
+
+        int leftA =
+
+    }
+}
+*/
---
+++ src/com/GFG/Searching/Q18_RepeatingElements_Naive.java
@@ -0,0 +1,30 @@
+package com.GFG.Searching;
+
+import java.util.Arrays;
+import java.util.Scanner;
+//This is a Naive method to find the repeating element in an array
+public class Q18_RepeatingElements_Naive {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements: ");//asking for size just to ease on taking input not using it
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+
+        for(int i = 0; i<size; i++){
+            System.out.print("Enter the " + (i+1) + " element: ");
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
+        System.out.println(findRepeatingElements(arr, size));
+    }
+    public static int findRepeatingElements(int[] arr, int size){
+        Boolean[] BoolArray = new Boolean[1000000];
+        for(int i = 0; i<size;i++){
+            if(BoolArray[arr[i]]){
+                return arr[i];
+            }
+            BoolArray[arr[i]]= true;
+        }
+        return -1;
+    }
+}
