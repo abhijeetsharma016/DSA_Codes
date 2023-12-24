0097fe56578b5b67777ddfa35e6885e440096dc8
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2023-12-24 19:08:20 +0530

    Q17_MedianOfTwoArray

--- src/com/GFG/Searching/Q17_MedianOfTwoSortedArray.java
+++ src/com/GFG/Searching/Q17_MedianOfTwoSortedArray.java
@@ -1,4 +1,3 @@
-/*
 package com.GFG.Searching;
 
 public class Q17_MedianOfTwoSortedArray {
@@ -8,19 +7,40 @@ public class Q17_MedianOfTwoSortedArray {
         System.out.println("Median of two arrays are: ");
         System.out.println(Median(arr1,arr2));
     }
-    public static int Median(int[] arr1, int[] arr2){
+    public static double Median(int[] arr1, int[] arr2){
         int size1 = arr1.length;
         int size2 = arr2.length;
+        int BmidImaginary = (size2+size1+1)/2;
         if(size1>size2){
-            return Median(arr2,arr1);//Swapping
-        }//2nd array should be greater than first one
-
-        int start = 0;
+            return Median(arr2,arr1);
+            //arr1 should be smaller than arr 2
+        }
+        int start=0;
         int end = size1;
-        int realMidInMergedArray = (size1+size2+1)/2;
+        while(start<=end){
+            int mid = (start+end)/2;
+            int Amid = mid;
+            int Bmid = BmidImaginary - mid;
 
-        int leftA =
+            int leftA = (Amid > 0) ? arr1[(Amid - 1)] : Integer.MIN_VALUE;
+            int leftB = (Bmid > 0) ? arr2[(Bmid - 1)] : Integer.MIN_VALUE;
+            int rightA = (Amid<size1)?arr1[Amid]:Integer.MAX_VALUE;
+            int rightB = (Bmid < size2) ? arr2[Bmid] : Integer.MAX_VALUE;
 
+            if (leftA <= rightB && leftB <= rightA) {
+                if ((size1 + size2) % 2 == 0) {
+                    return ((Math.max(leftA,leftB)+Math.min(rightA,rightB))/2.0);
+                }
+                else
+                    return Math.max(leftA,leftB);
+
+            } else if (leftA>rightB) {
+                end = mid-1;
+            }
+            else {
+                start=mid+1;
+            }
+        }
+        return  0.0;
     }
 }
-*/
