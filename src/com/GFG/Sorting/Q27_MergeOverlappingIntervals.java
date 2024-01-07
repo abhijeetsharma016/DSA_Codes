551cefdadb09e3d5a702f6977004d275892ae2d0
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-01-07 22:29:29 +0530

    21

--- src/com/GFG/Sorting/Q27_MergeOverlappingIntervals.java
+++ src/com/GFG/Sorting/Q27_MergeOverlappingIntervals.java
@@ -1,4 +1,7 @@
 package com.GFG.Sorting;
 
-public class Q27_MergeOverlappingIntervals {
+public class Q27_MergeOverlappingIntervals            {
+    public static void main(String[] args) {
+
+    }
 }
