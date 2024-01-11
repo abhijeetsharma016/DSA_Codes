c0211a023eb358adb6220f7272a4e0f9c6ae5ba8
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-01-12 00:25:15 +0530

    Q2_MatrixBoundryTraversal

--- src/com/GFG/Matrix/Q2_MatrixBoundaryTraversal.java
+++ src/com/GFG/Matrix/Q2_MatrixBoundaryTraversal.java
@@ -1,4 +1,36 @@
 package com.GFG.Matrix;
 
 public class Q2_MatrixBoundaryTraversal {
+    static int R = 4,C = 4;
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+        printBoundry(matrix);
+    }
+
+    public static void printBoundry(int[][] matrix) {
+         if(R == 1){
+             for (int i = 0; i < C; i++) {
+                 System.out.println(matrix[0][i]);
+             }
+        } else if (C==1) {
+             for (int i = 0; i < R; i++) {
+                 System.out.println(matrix[i][0]);
+             }
+         }
+        for (int i = 0; i < C; i++) {
+            System.out.print(matrix[0][i]);
+        }
+        System.out.println("");
+        for (int j = 1; j < R; j++) {
+            System.out.println(matrix[j][C-1]);
+        }
+        for (int i = 0; i < C-1; i++) {
+            System.out.print(matrix[R-1][i]);
+        }
+        System.out.println("");
+        for (int j = 1; j < R; j++) {
+            System.out.println(matrix[j][0]);
+        }
+
+    }
 }
