a1b55234d8b93c0524e8b1d9047dee53edb17323
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-01-13 16:17:21 +0530

    Q6_SearchingInAMatrix

---
+++ src/com/GFG/Matrix/Q6_SearchInASortedMatrix.java
@@ -0,0 +1,25 @@
+package com.GFG.Matrix;
+
+public class Q6_SearchInASortedMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {{10, 20, 30, 40},
+                {15, 25, 35, 45},
+                {27, 29, 37, 48},
+                {32, 33, 39, 50}};
+        int x = 29;
+        searchMatrix(matrix, x);
+    }
+
+    public static void searchMatrix(int[][] matrix, int x) {
+        int i = 0, j = matrix[0].length-1;
+        while (matrix[i][j] != x && i < matrix.length && j > -1) {
+            if(matrix[i][j]>x){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        System.out.println("(" + i + "," + j + ")");
+    }
+}
