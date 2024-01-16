2a6aec604ff4d3e3843475230f45a67f4d7e2b4c
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-01-16 18:43:58 +0530

    Hash using linear Probing

---
+++ src/com/GFG/Hashing/Q2_OpenAddressingUsingLinerProbing.java
@@ -0,0 +1,73 @@
+package com.GFG.Hashing;
+
+import java.util.ArrayList;
+
+public class Q2_OpenAddressingUsingLinerProbing {
+    public static void main(String[] args) {
+        Hash hashTable = new Hash(10);
+        // Insert elements into the hash table
+        hashTable.Insert(5);
+        hashTable.Insert(25);
+        hashTable.Insert(15);
+        hashTable.display();
+
+        System.out.println(hashTable.search(5));
+    }
+}
+
+class Hash{
+    ArrayList <Integer> table;
+    int Bucket;
+    public Hash(int b){
+        Bucket = b;
+        table = new ArrayList<>();
+        //Initialise table with -1 to represent empty slots
+        for (int i = 0; i < b; i++) {
+            table.add(-1);
+        }
+    }
+
+    public void Insert(int element) {
+        int index = element%Bucket;
+        if (table.get(index) == -1 )
+            table.set(index, element);
+
+        else{
+            int originalIndex = index;
+            index = (index+1)%Bucket;
+            while (table.get(index) != -1 && index != originalIndex) {
+                index = (index+1) % Bucket;
+            }
+            if(index == originalIndex){
+                System.out.println("Hashset full");
+            }
+            else {
+                table.set(index, element);
+            }
+        }
+    }
+    public boolean search(int element) {
+        int index = element % Bucket;
+        if (table.get(index) == element) {
+            return true;
+        } else {
+            int originalIndex = index;
+            index = (index+1)%Bucket;
+            while (table.get(index) != index && index != originalIndex) {
+                index = (index + 1) % Bucket;
+            }
+            if(index == originalIndex) {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+    }
+    public void display() {
+        System.out.println("Hash Table:");
+        for (int i = 0; i < Bucket; i++) {
+            System.out.println(i + ": " + table.get(i));
+        }
+    }
+}
