52c23a683f61dba1d9a176b152c55c7db0ae8de2
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-01-17 19:34:48 +0530

    added display and search

--- src/com/GFG/Hashing/Q4_DoubleHashing.java
+++ src/com/GFG/Hashing/Q4_DoubleHashing.java
@@ -13,6 +13,8 @@ public class Q4_DoubleHashing {
         myhash.insert(60);
         myhash.insert(70);
         myhash.insert(80);
+        myhash.display();
+        System.out.println(myhash.search(70));
     }
 }
 class DoubleHashing {
@@ -52,5 +54,39 @@ class DoubleHashing {
         }
     }
 
+    public void display() {
+        System.out.println("Hash Table:");
+        for (int i = 0; i < Bucket1; i++) {
+            System.out.println(i + ": " + myArrayList.get(i));
+        }
+    }
 
+    public boolean search(int element) {
+        int index1 = element%Bucket1;
+        if (myArrayList.get(index1) == element) {
+            location = index1;
+            return true;
+        } else {
+            int original = index1;
+            int index2 = element % Bucket2;
+            int hash = (index1 + index2) % Bucket1;
+            int i = 2;
+            while (hash != original && myArrayList.get(hash) != -1) {
+                hash = (index1 + i * index2);
+                i++;
+            }
+            if (myArrayList.get(hash) == -1) {
+                return false;
+            } else {
+                location = hash;
+                return true;
+            }
+        }
+    }
+
+    public void delete(int element) {
+        if (search(element)) {
+
+        }
+    }
 }
