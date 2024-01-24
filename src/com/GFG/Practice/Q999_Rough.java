12c9cbf6cac3f56b8a7578d88142fb59635e6b00
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-01-24 22:42:18 +0530

    Rouch updated

--- src/com/GFG/Practice/Q999_Rough.java
+++ src/com/GFG/Practice/Q999_Rough.java
@@ -2,6 +2,6 @@ package com.GFG.Practice;
 
 public class Q999_Rough {
     public static void main(String[] args) {
-        System.out.println(9/0);
+        System.out.println((char)(9 + '0'));
     }
 }
