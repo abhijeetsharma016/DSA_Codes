84b43b676951b258fada5aada9586fcd9d599fae
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-02-06 21:39:21 +0530

    rough

--- src/com/GFG/Practice/Q999_Rough.java
+++ src/com/GFG/Practice/Q999_Rough.java
@@ -2,7 +2,7 @@ package com.GFG.Practice;
 
 public class Q999_Rough {
     public static void main(String[] args) {
-        for(int i=0; i<5;i++){
+        for(int i=0; i<5;){
             System.out.print("hi" + i);
             i++;
 
