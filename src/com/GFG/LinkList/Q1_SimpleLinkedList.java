8202a8d78b902f91903eb5bfc928c6ef0fb2b2ff
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-02-17 14:34:07 +0530

    Q1_SimpleLinkedList implementation

--- src/com/GFG/LinkList/Q1_SimpleLinkedList.java
+++ src/com/GFG/LinkList/Q1_SimpleLinkedList.java
@@ -1,4 +1,20 @@
 package com.GFG.LinkList;
 
 public class Q1_SimpleLinkedList {
+    public static void main(String[] args){
+        Node head = new Node(10);
+        Node temp1 = new Node(20);
+        Node temp2 = new Node(30);
+        head.next = temp1;
+        temp1.next = temp2;
+        System.out.print(head.Data+"-->"+temp1.Data+"-->"+temp2.Data);
+    }
+}
+class Node{
+    int Data;
+     Node next;
+    Node(int Data){
+        this.Data = Data;
+        next = null;
+    }
 }
