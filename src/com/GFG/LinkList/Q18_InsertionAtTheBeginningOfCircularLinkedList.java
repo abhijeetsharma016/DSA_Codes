d632248d07b2ac82e45b4048999c471a80a432f8
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-02-25 23:26:44 +0530

    insert at end

---
+++ src/com/GFG/LinkList/Q18_InsertionAtTheBeginningOfCircularLinkedList.java
@@ -0,0 +1,51 @@
+package com.GFG.LinkList;
+
+
+
+class Node18 {
+    int data;
+    Node18 next;
+    Node18(int d){
+        data=d;
+        next=null;
+    }
+}
+
+class Q18_InsertionAtTheBeginningOfCircularLinkedList {
+
+    public static void main(String args[])
+    {
+        Node18 head=new Node18(10);
+        head.next=new Node18(20);
+        head.next.next=new Node18(30);
+        head.next.next.next=head;
+        head=insertBegin(head,15);
+        printlist(head);
+
+    }
+
+    public static void printlist(Node18 head){
+        if(head==null)return;
+        Node18 r=head;
+        do{
+            System.out.print(r.data+" ");
+            r=r.next;
+        }while(r!=head);
+    }
+
+    static Node18 insertBegin(Node18 head, int x){
+        Node18 temp=new Node18(x);
+        if(head==null){
+            temp.next=temp;
+            return temp;
+        }
+        else{
+            temp.next=head.next;
+            head.next=temp;
+            int t=head.data;
+            head.data=temp.data;
+            temp.data=t;
+            return head;
+        }
+    }
+}
--- src/com/GFG/LinkList/Q19_InsertionAtEndInCircularLinkedList.java
+++ src/com/GFG/LinkList/Q19_InsertionAtEndInCircularLinkedList.java
@@ -1,39 +1,41 @@
 package com.GFG.LinkList;
 
 
-class Node18 {
+class Node19 {
     int data;
-    Node18 next;
-    Node18(int d){
+    Node19 next;
+    Node19(int d){
         data=d;
         next=null;
     }
 }
 
-public class Q18_InsertionAtBeginningInCircularLinkedList {
+
+
+public class Q19_InsertionAtEndInCircularLinkedList {
 
     public static void main(String args[])
     {
-        Node18 head=new Node18(10);
-        head.next=new Node18(20);
-        head.next.next=new Node18(30);
+        Node19 head=new Node19(10);
+        head.next=new Node19(20);
+        head.next.next=new Node19(30);
         head.next.next.next=head;
         head=insertEnd(head,15);
         printlist(head);
 
     }
 
-    public static void printlist(Node18 head){
+    public static void printlist(Node19 head){
         if(head==null)return;
-        Node18 r=head;
+        Node19 r=head;
         do{
             System.out.print(r.data+" ");
             r=r.next;
         }while(r!=head);
     }
 
-    static Node18 insertEnd(Node18 head, int x){
-        Node18 temp=new Node18(x);
+    static Node19 insertEnd(Node19 head, int x){
+        Node19 temp=new Node19(x);
         if(head==null){
             temp.next=temp;
             return temp;
