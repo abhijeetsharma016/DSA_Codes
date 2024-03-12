ac15ba123755d938786011911fcae079bd423193
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-03-12 19:38:07 +0530

    Q4_BalancedParenthesis

---
+++ src/com/GFG/Stack/Q4_BalancedParenthesis.java
@@ -0,0 +1,45 @@
+package com.GFG.Stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Q4_BalancedParenthesis {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string with parentheses: ");
+
+        // Check if there is any input available
+        if (scanner.hasNextLine()) {
+            String s = scanner.nextLine();
+            if (isBalanced(s)) {
+                System.out.println("The parentheses in the string are balanced.");
+            } else {
+                System.out.println("The parentheses in the string are not balanced.");
+            }
+        }
+    }
+
+    static boolean isBalanced(String s) {
+        Stack<Character> st = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                st.push(c);
+            }
+            else{
+                if (st.empty()) {
+                    return false;
+                } else if (c == ')' && st.peek() != '(') {
+                    return false;
+                } else if (c == ']' && st.peek() != '[') {
+                    return false;
+                } else if (c == '}' && st.peek() != '{') {
+                    return false;
+                }
+                else{
+                    st.pop();
+                }
+            }
+        }
+        return true;
+    }
+}
