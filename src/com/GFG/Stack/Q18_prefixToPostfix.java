f49197a9176f1a95894b04107a4d67f7ef0f0dcf
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-04-10 23:29:17 +0530

    Q18_prefixToPostfix

--- src/com/GFG/Stack/Q18_prefixToPostfix.java
+++ src/com/GFG/Stack/Q18_prefixToPostfix.java
@@ -1,4 +1,33 @@
 package com.GFG.Stack;
 
-public class Q18_prefixToPostfix {
+
+import java.util.*;
+        import java.io.*;
+        import java.lang.*;
+
+class Q18_prefixToPostfix {
+
+
+
+    static void fillLPS(String str, int lps[]){
+        int n=str.length(),len=0;
+        lps[0]=0;
+        int i=1;
+        while(i<n){
+            if(str.charAt(i)==str.charAt(len))
+            {len++;lps[i]=len;i++;}
+            else
+            {if(len==0){lps[i]=0;i++;}
+            else{len=lps[len-1];}
+            }
+        }
+    }
+
+    public static void main(String args[])
+    {   String txt = "abacabad";int[] lps=new int[txt.length()];
+        fillLPS(txt,lps);
+        for(int i=0;i<txt.length();i++){
+            System.out.print(lps[i]+" ");
+        }
+    }
 }
