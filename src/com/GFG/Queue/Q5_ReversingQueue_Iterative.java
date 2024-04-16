539edb7444cef302fd9db0b2fa1bcbbba491f994
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-04-16 19:47:00 +0530

    Q5_ReversingQueue_Iterative

---
+++ src/com/GFG/Queue/Q5_ReversingQueue_Iterative.java
@@ -0,0 +1,43 @@
+package com.GFG.Queue;
+
+
+    import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+
+    public class Q5_ReversingQueue_Iterative {
+
+        static Queue<Integer> queue;
+
+
+        static void Print() {
+            for (Integer x : queue)
+                System.out.print(x + " ");
+        }
+
+
+        static void reversequeue() {
+            Stack<Integer> stack = new Stack<>();
+            while (!queue.isEmpty()) {
+                stack.add(queue.peek());
+                queue.remove();
+            }
+            while (!stack.isEmpty()) {
+                queue.add(stack.peek());
+                stack.pop();
+            }
+        }
+
+
+        public static void main(String args[]) {
+            queue = new LinkedList<Integer>();
+            queue.add(12);
+            queue.add(5);
+            queue.add(15);
+            queue.add(20);
+
+            reversequeue();
+            Print();
+        }
+    }
