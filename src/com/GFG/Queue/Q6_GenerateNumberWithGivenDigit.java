a20a25ac9013e5ef556816f9af24aa196f57947e
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-04-16 20:03:50 +0530

    Q6_GenerateNumberWithGivenDigit

---
+++ src/com/GFG/Queue/Q6_GenerateNumberWithGivenDigit.java
@@ -0,0 +1,35 @@
+package com.GFG.Queue;
+
+    import java.util.LinkedList;
+import java.util.*;
+
+
+public class Q6_GenerateNumberWithGivenDigit {
+
+
+    static void printFirstN(int n) {
+        Queue<String> q = new LinkedList<>();
+
+        q.add("5");
+        q.add("6");
+
+        for (int i = 0; i < n; i++) {
+            String curr = q.poll();
+
+            System.out.print(curr + " ");
+
+            q.add(curr + "5");
+            q.add(curr + "6");
+        }
+
+    }
+
+
+    public static void main(String args[]) {
+        int n = 5;
+
+        printFirstN(n);
+    }
+}
+
+
