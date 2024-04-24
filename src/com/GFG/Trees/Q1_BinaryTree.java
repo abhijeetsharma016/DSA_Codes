55c6f98ae50f7763d805cb140effe095abb5d36c
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-04-24 19:06:27 +0530

    Q1_BinaryTree

---
+++ src/com/GFG/Trees/Q1_BinaryTree.java
@@ -0,0 +1,25 @@
+package com.GFG.Trees;
+
+import java.lang.*;
+
+class Node
+{
+    int key;
+    Node left;
+    Node right;
+    Node(int k){
+        key=k;
+        left=right=null;
+    }
+}
+
+class Q1_BinaryTree {
+
+    public static void main(String args[])
+    {
+        Node root=new Node(10);
+        root.left=new Node(20);
+        root.right=new Node(30);
+        root.left.left=new Node(40);
+    }
+}
